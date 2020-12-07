2fa04e75be5de5e7fb6847be37c4d1e98f5b6db1
AmandineGervis <gervis.amandine@gmail.com>
2020-12-07 16:50:04 +0100

    page home css and little animation for train

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,82 @@
-import React from "react";
-import Footer from "./Footer";
-import Header from "./Header";
-import "../style/Home.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from './Footer';
+import Header from './Header';
+import '../style/Home.css';
+import CalendrierHome from '../img/calendrier:HOME.png';
+import Formejaune from '../img/forme2:home.png';
+import FormeBig from '../img/forme1:home.png';
+import ImageFille from '../img/fillette:home.png';
+import FormeRouge from '../img/forme3:home.png';
+import Image2 from '../img/image2.png';
+import Chiffre from '../img/chiffre.png';
+import Simulator from '../img/simulator.png';
+import Train from '../img/train.png';
+import Bandeau from '../img/bandeau.png';
+import '../style/Footer.css';
 
 const Home = () => {
   return (
     <>
       <Header />
       <main>
-        <section className="container-reservation">
-          <div className="section-reservation">
-            <h1>Et si c'était juste pour une heure ?</h1>
-            <h2>
-              Trouvez et réservez{" "}
-              <span style={{ color: "red" }}>rapidement</span> votre place en
-              crèche !
-            </h2>
-            <button className="btn-home-reservation">Je réserve !</button>
-            <button className="btn-home-calendar"> Calendrier</button>
-          </div>
-          <button className="btn-home-espace-pro">Espace professionnel</button>
-        </section>
-        <section className="container-saviezvous">
-          <div className="image-saviezvous"></div>
-          <div className="section-saviezvous">
-            <h1>Le saviez-vous ?</h1>
-            <div className="section-stats-saviezvous">
-              {/* <p className='stats'>12%</p>
-            <p className='stats'>333<br/><span>millions</span></p>
-            <p className='stats'>63%</p>
-            <p className='p-stats'>Des enfants (de 10 semianes à 3 ans) ont une place en crèche</p>
-            <p className='p-stats'>d'heures d'accueil disponibles en France chaque année</p>
-            <p className='p-stats'>Le taux d'occupation réel moyen des crèches</p> */}
-            </div>
-          </div>
-        </section>
+        <img className='formeBig' src={FormeBig} alt='forme jaune 2' />
+        <img className='imgFille' src={ImageFille} alt='fillette' />
+        <img className='formeJaune' src={Formejaune} alt='forme jaune' />
+        <img className='formeRouge' src={FormeRouge} alt='forme rouge' />
+
+        <div className='containerText'>
+          <h1 className='titre1'>Et si c'était</h1>
+          <div className='ligne1'></div>
+          <h1 className='titre2'>juste une heure?</h1>
+          <div className='ligne2'></div>
+          <p className='soustitre'>
+            Trouvez et reservez <span className='rapidement'>rapidement</span>{' '}
+            votre place en crèche !
+          </p>
+          <button className='reserver'>Je réserve!</button>
+          <button className='planning'></button>
+          <img
+            className='planningIcone'
+            src={CalendrierHome}
+            alt='icone du planning'
+          />
+        </div>
       </main>
+      <img className='fillette2' src={Image2} alt='fille 2' />
+      <h1 className='saviezVous'>Le saviez-vous?</h1>
+      <div className='ligneSaviez'></div>
+      <img className='chiffre' src={Chiffre} alt='chiffre' />
+      <p className='textJaune'>
+        Des enfants{' '}
+        <span className='soustitre2'>(de 10 semaines à 3 ans) </span>ont une
+        place en crèche
+      </p>
+      <p className='textBleu'>Le taux d’occupation réel moyen des crèches</p>
+      <p className='textRouge'>
+        <span className='hour'>d’heures</span> d’accueil disponibles en France
+        chaque année
+      </p>
+
+      <div className='containerSimulator'>
+        <h1 className='simulatorTitle'>Simulateur de coût</h1>
+        <div className='ligne3'></div>
+        <p className='soustitreSimulator'>
+          Estimez et découvrez le <span className='price'>prix</span> de votre
+          place en crèche !
+        </p>
+        <Link className='simulatorLink' to='/ensavoirplus'>
+          En savoir plus
+        </Link>
+      </div>
+      <img className='simulator' src={Simulator} alt='simulateur' />
+      <img className='train' src={Train} alt='train' />
+      <img className='bandeau' src={Bandeau} alt='Beandeau' />
+      <div class='water'>
+        <div class='ripple-one'></div>
+        <div class='ripple-two'></div>
+        <div class='ripple-three'></div>
+      </div>
       <Footer />
     </>
   );
