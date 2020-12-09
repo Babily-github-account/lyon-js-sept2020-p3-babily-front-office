cec2ff00f45b1a9f85176d14a2c252724a9cf2ec
gclement28 <clementgarcin6@gmail.com>
2020-12-09 16:53:51 +0100

    designed contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,11 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { EmailIcon, EmailShareButton } from "react-share";
+import fillevole from "../img/fillevole.jpeg";
+import Cafe from "../img/café.png";
+import IconeEnvoi from "../img/icone.png";
+import Facetime from "../img/facetime.png";
+import MainsSerrees from "../img/main.png";
 
 const Contact = () => {
   const [inputs, setInputs] = useState({
@@ -37,79 +42,111 @@ const Contact = () => {
   return (
     <div>
       <div className="containerForm">
-        <h1 className="titre">Contactez-nous</h1>
+        <img className="fillevole" src={fillevole} alt="forme jaune 2" />
+
         <form onSubmit={handleSubmit(onSubmit)} className="form">
-          <div className="bloc1">
-            <input
-              className="contact-input"
-              onChange={(e) => handleInputChange(e)}
-              type="text"
-              facebook
-              id="input-name"
-              placeholder="Nom"
-              ref={register}
-              value={inputs.name}
-              name="name"
-            />
+          <div className="container-bloc1">
+            <div className="box1">
+              <input
+                className="box"
+                onChange={(e) => handleInputChange(e)}
+                type="text"
+                facebook
+                id="input-name"
+                placeholder="Nom"
+                ref={register}
+                value={inputs.name}
+                name="name"
+              />
+
+              <input
+                className="box"
+                placeholder="Email"
+                onChange={(e) => handleInputChange(e)}
+                value={inputs.email}
+                name="email"
+                type="email"
+                maxLength="50"
+                required
+                ref={register}
+              />
+              <input
+                className="box"
+                onChange={(e) => handleInputChange(e)}
+                type="text"
+                id="input-subject"
+                placeholder="Sujet"
+                name="subject"
+                ref={register}
+                value={inputs.sujet}
+              />
+            </div>
+            <div className="box2">
+              <textarea
+                className="box"
+                onChange={(e) => handleInputChange(e)}
+                name="message"
+                type="text"
+                id="input-message"
+                placeholder="Message"
+                ref={register}
+                value={inputs.message}
+              ></textarea>
+            </div>
+          </div>
+          <div className="container-bloc2">
+            <div className="box3">
+              <select className="box" ref={register}>
+                <option value="Parents">Parents</option>
+                <option value="Professionnel">
+                  Professionnel de la petite enfance
+                </option>
+                <option selected value="Employeur">
+                  Employeur
+                </option>
+                <option value="Autre">Autre</option>
+              </select>
+            </div>
+            <div className="section-rendezvous">
+              <EmailShareButton>
+                <img
+                  src={IconeEnvoi}
+                  alt="Envoyer un mail"
+                  className="email-share-button"
+                />
+              </EmailShareButton>
+              <EmailShareButton>
+                <img
+                  src={Cafe}
+                  alt="Prendre un Café"
+                  className="email-share-button"
+                />
+              </EmailShareButton>
+              <EmailShareButton>
+                <img
+                  src={Facetime}
+                  alt="Rendez-vous en Facetime"
+                  className="email-share-button facetime"
+                />
+              </EmailShareButton>
+              <EmailShareButton>
+                <img
+                  src={MainsSerrees}
+                  alt="Rendez-vous en présentiel"
+                  className="email-share-button mainsserrees"
+                />
+              </EmailShareButton>
+            </div>
 
             <input
-              className="contact-input"
-              placeholder="Email"
-              onChange={(e) => handleInputChange(e)}
-              value={inputs.email}
-              name="email"
-              type="email"
-              maxLength="50"
-              required
-              ref={register}
+              className="box"
+              type="submit"
+              value="Envoyer"
+              id="input-submit"
             />
-            <input
-              className="contact-input"
-              onChange={(e) => handleInputChange(e)}
-              type="text"
-              id="input-subject"
-              placeholder="Sujet"
-              name="subject"
-              ref={register}
-              value={inputs.sujet}
-            />
+            
+            
           </div>
-          <div className="Bloc2">
-            <select className="contact-input" ref={register}>
-              <option value="Parents">Parents</option>
-              <option value="Professionnel">
-                Professionnel de la petite enfance
-              </option>
-              <option selected value="Employeur">
-                Employeur
-              </option>
-              <option value="Autre">Autre</option>
-            </select>
-          </div>
-          <div className="bloc3">
-            <textarea
-              className="contact-input"
-              onChange={(e) => handleInputChange(e)}
-              name="message"
-              type="text"
-              id="input-message"
-              placeholder="Message"
-              ref={register}
-              value={inputs.message}
-            ></textarea>
-          </div>
-          <input
-            className="contact-input"
-            type="submit"
-            value="Envoyer"
-            id="input-submit"
-          />
-          <EmailShareButton
-            url={"https://www.gmail.com/"}
-            className="email-share-button"
-          >
-            <EmailIcon size={50} borderRadius={50}></EmailIcon>
-          </EmailShareButton>
         </form>
       </div>
       <div className="contactAnim">
