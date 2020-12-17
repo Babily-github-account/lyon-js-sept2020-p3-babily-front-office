c24c95ed489697972ab81e2c1e71e2853d1e31fd
gclement28 <clementgarcin6@gmail.com>
2020-12-17 11:49:25 +0100

    footer is responsive on every device

--- components/Footer.js
+++ components/Footer.js
@@ -1,86 +1,48 @@
 import { RiFacebookBoxLine, RiLinkedinBoxLine } from 'react-icons/ri';
 import { FaTwitterSquare } from 'react-icons/fa';
 import Link from 'next/link';
+import styles from './Footer.module.css';
 
 export default function Footer() {
-  const containerFooter = {
-    display: 'flex',
-    alignItems: 'flex-end',
-    justifyContent: 'space-around',
-    backgroundImage: `url(/images/FooterWaves.svg)`,
-    backgroundSize: '100%',
-    backgroundRepeat: 'no-repeat',
-
-    height: '400px',
-    width: '100%',
-    color: 'white',
-    fontFamily: "'Montserrat Alternates', sans-serif",
-  };
-
-  const containerReseaux = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    paddingBottom: '20px',
-  };
-  const titleFooter = {
-    fontSize: '24px',
-    margin: '0',
-  };
-
-  const followFooter = {
-    fontSize: '10px',
-  };
-
-  const footerNav = {
-    width:'50%',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    listStyle: 'none',
-    justifyContent: 'space-around',
-    paddingBottom: '80px',
-  };
-
-  const containerAdress = {
-    fontSize: '12px',
-    paddingBottom: '40px',
-  };
-
   return (
-    <div style={containerFooter}>
-      <div style={containerReseaux}>
-        <h5 style={titleFooter}>Babily.fr</h5>
-        <p style={followFooter}>Suivez-nous !</p>
-        <div>
+    <div className={styles.containerFooter}>
+      <div className={styles.containerReseaux}>
+        <h5 className={styles.titleFooter}>Babily.fr</h5>
+        <p className={styles.followFooter}>Suivez-nous !</p>
+        <div className={styles.iconsSocial}>
           <RiFacebookBoxLine />
           <FaTwitterSquare />
           <RiLinkedinBoxLine />
         </div>
       </div>
 
-      <ul style={footerNav}>
+      <div className={styles.footerNav}>
         <Link href="/decouvrir">
           <a>Découvrir</a>
         </Link>
+
         <Link href="/votrespace">
           <a>Votre espace</a>
-        </Link>{' '}
+        </Link>
+
         <Link href="/temoignages">
           <a>Témoignages</a>
         </Link>
+
         <Link href="/contact">
           <a>Contact </a>
         </Link>
+
         <Link href="/confidentialites">
           <a>Confidentialités</a>
         </Link>
+
         <Link href="/mentions">
           <a>Mentions</a>
         </Link>
-      </ul>
+      </div>
 
-      <div style={containerAdress}>
+      <div className={styles.containerAdress}>
         <p>61 Cours de la Liberté</p>
         <p>69003 Lyon </p>
         <p>Nicolas Lorut</p>
