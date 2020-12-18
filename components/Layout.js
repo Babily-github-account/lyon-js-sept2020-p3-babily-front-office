4e211a1b186789e52c9083acd605ca0698fefafc
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2020-12-18 09:44:00 +0100

    fixes

--- components/Layout.js
+++ components/Layout.js
@@ -11,6 +11,7 @@ export default function Layout({ children, pageTitle }) {
     <>
       <Head>
         <title>{pageTitle ? `${pageTitle} - ` : ''}Babily</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <GoogleFonts href="https://fonts.googleapis.com/css2?family=Krub:wght@700&family=Montserrat+Alternates:ital,wght@0,800;1,600&display=swap" />
       <NextNprogress
---
+++ pages/espacepro.js
@@ -0,0 +1,3 @@
+export default function EspacePro() {
+  return <>Espace Pro</>;
+}
