dd347196a108a52a5588977c5710f8e58999cfd7
SaKara69 <sabrina.karakog@gmail.com>
2021-01-06 11:32:22 +0100

    rectif background footer

--- pages/concept.js
+++ pages/concept.js
@@ -16,7 +16,6 @@ export default function concept() {
     filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
   };
   const conceptStart = {
-    backgroundColor: '#faf5f0',
     width: '100%',
   };
 
