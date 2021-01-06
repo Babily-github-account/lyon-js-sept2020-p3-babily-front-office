822fe1b7b6b6b997d87dafe88c16aaaf1b873cc8
SaKara69 <sabrina.karakog@gmail.com>
2021-01-06 16:50:50 +0100

    remplacement des images par code svg

--- components/LeSaviezVous.js
+++ components/LeSaviezVous.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
-import Image from 'next/image';
 
 export default function LeSaviezVous() {
   const heuresDisponibles = useSpring({ from: { val: 0 }, to: { val: 330 } });
@@ -20,7 +19,7 @@ export default function LeSaviezVous() {
     textDecorationStyle: 'solid',
     textDecorationColor: '#fa6870',
     textDecorationThickness: '15px',
-    paddingBottom: '7%',
+    paddingBottom: '50px',
   };
 
   const notions = {
@@ -28,19 +27,83 @@ export default function LeSaviezVous() {
     flexDirection: 'row',
     flexWrap: 'wrap',
     textAlign: 'center',
+    minHeight: '',
   };
 
   const notion = {
     position: 'relative',
-    margin: '50px auto',
+    margin: '5% auto',
   };
-  const palette = {
+  const paletteA1 = {
     margin: 'auto',
+    fill: 'rgba(250,104,112,1)',
+    position: 'absolute',
+    width: '53%',
+    minWidth: '200px',
+    height: 'auto',
+    top: '50%',
+    bottom: '50%',
+    transform: 'translate(-50%, 2%)',
+  };
+  const paletteB1 = {
+    margin: 'auto',
+    fill: 'rgba(250,104,112,0.5)',
+    position: 'absolute',
+    width: '53%',
+    minWidth: '200px',
+    height: 'auto',
+    transform: 'translate(-45%, 4%) rotate(-30deg)',
+    top: '50%',
+    bottom: '50%',
+  };
+  const paletteA2 = {
+    margin: 'auto',
+    fill: 'rgba(255,187,105,1)',
+    position: 'absolute',
+    width: '53%',
+    minWidth: '200px',
+    height: 'auto',
+    top: '50%',
+    bottom: '50%',
+    transform: 'translate(-60%, -5%) rotate(55deg)',
+  };
+  const paletteB2 = {
+    margin: 'auto',
+    fill: 'rgba(255,187,105,0.5)',
+    position: 'absolute',
+    width: '53%',
+    minWidth: '200px',
+    height: 'auto',
+    top: '50%',
+    bottom: '50%',
+    transform: 'translate(-60%, -10%) rotate(110deg)',
+  };
+  const paletteA3 = {
+    margin: 'auto',
+    fill: 'rgba(60,132,251,1)',
+    position: 'absolute',
+    width: '69%',
+    minWidth: '200px',
+    height: 'auto',
+    top: '50%',
+    bottom: '50%',
+    transform: 'translate(-55%, -5%) rotate(120deg)',
+  };
+  const paletteB3 = {
+    margin: 'auto',
+    fill: 'rgba(60,132,251,0.5)',
+    position: 'absolute',
+    width: '69%',
+    minWidth: '200px',
+    height: 'auto',
+    top: '50%',
+    bottom: '50%',
+    transform: 'translate(-55%, 5%) rotate(40deg)',
   };
   const chiffreValeur1 = {
-    position: 'absolute',
-    width: '300px',
-    height: '300px',
+    position: 'relative',
+    width: '100%',
+    height: '100%',
     textAlign: 'center',
     top: '25%',
     left: '50%',
@@ -49,9 +112,9 @@ export default function LeSaviezVous() {
   };
 
   const chiffreValeur = {
-    position: 'absolute',
-    width: '300px',
-    height: '300px',
+    position: 'relative',
+    width: '100%',
+    height: '100%',
     textAlign: 'center',
     top: '30%',
     left: '50%',
@@ -65,11 +128,11 @@ export default function LeSaviezVous() {
   const valeur = {
     fontSize: '1.6rem',
   };
-  const texte = {
-    marginTop: '1.2rem',
-    paddingLeft: '20%',
-    paddingRight: '20%',
+  const texteExplicatif = {
+    padding: '10% 5%',
     fontFamily: 'Krub, sans-serif',
+    marginTop: '50px',
+    color: 'black',
   };
 
   const heures = {
@@ -82,58 +145,57 @@ export default function LeSaviezVous() {
       <h3 style={titreSaviezVous}>Le saviez-vous?</h3>
       <div style={notions}>
         <div style={notion}>
-          <Image
-            src="/images/paletteRouge.png"
-            alt="palette rouge"
-            width={300}
-            height={300}
-            style={palette}
-          />
+          <svg viewBox="0 0 50 50" style={paletteA1}>
+            <path d="M46.5,11.5c3.7,18.6-2.5,32-16.3,32S2.7,33,.4,21.2,5.3,4.9,18.9,2.2C29.8.1,43.5-4.2,46.5,11.5Z" />
+          </svg>
+          <svg viewBox="0 0 50 50" style={paletteB1}>
+            <path d="M46.5,11.5c3.7,18.6-2.5,32-16.3,32S2.7,33,.4,21.2,5.3,4.9,18.9,2.2C29.8.1,43.5-4.2,46.5,11.5Z" />
+          </svg>
           <div style={chiffreValeur1}>
             <animated.span style={nombre}>
               {heuresDisponibles.val.interpolate((val) => Math.floor(val))}
             </animated.span>
             <p style={valeur}>millions</p>
+            <p style={texteExplicatif}>
+              D'<span style={heures}>heures</span> d'accueil disponibles en
+              France chaque année 12 %
+            </p>
           </div>
-          <p style={texte}>
-            D'<span style={heures}>heures</span> d'accueil disponibles en France
-            chaque année 12 %
-          </p>
         </div>
 
         <div style={notion}>
-          <Image
-            src="/images/paletteJaune.png"
-            alt="palette jaune"
-            width={300}
-            height={300}
-            style={palette}
-          />
+          <svg viewBox="0 0 50 50" style={paletteA2}>
+            <path d="M46.5,11.5c3.7,18.6-2.5,32-16.3,32S2.7,33,.4,21.2,5.3,4.9,18.9,2.2C29.8.1,43.5-4.2,46.5,11.5Z" />
+          </svg>
+          <svg viewBox="0 0 50 50" style={paletteB2}>
+            <path d="M46.5,11.5c3.7,18.6-2.5,32-16.3,32S2.7,33,.4,21.2,5.3,4.9,18.9,2.2C29.8.1,43.5-4.2,46.5,11.5Z" />
+          </svg>
           <div style={chiffreValeur}>
             <animated.span style={nombre}>
               {placesEnCreche.val.interpolate((val) => Math.floor(val))}
             </animated.span>
             <span style={valeur}>%</span>
+            <p style={texteExplicatif}>
+              Des enfants (de 10 semaines à 3 ans) ont une place en crèche
+            </p>
           </div>
-          <p style={texte}>
-            Des enfants (de 10 semaines à 3 ans) ont une place en crèche
-          </p>
         </div>
         <div style={notion}>
-          <Image
-            src="/images/paletteBleue.png"
-            alt="palette bleue"
-            width={300}
-            height={300}
-            style={palette}
-          />
+          <svg viewBox="0 0 50 50" style={paletteA3}>
+            <path d="M46.5,11.5c3.7,18.6-2.5,32-16.3,32S2.7,33,.4,21.2,5.3,4.9,18.9,2.2C29.8.1,43.5-4.2,46.5,11.5Z" />
+          </svg>
+          <svg viewBox="0 0 50 50" style={paletteB3}>
+            <path d="M46.5,11.5c3.7,18.6-2.5,32-16.3,32S2.7,33,.4,21.2,5.3,4.9,18.9,2.2C29.8.1,43.5-4.2,46.5,11.5Z" />
+          </svg>
           <div style={chiffreValeur}>
             <animated.span style={nombre}>
               {tauxOccupation.val.interpolate((val) => Math.floor(val))}
             </animated.span>
             <span style={valeur}>%</span>
+            <p style={texteExplicatif}>
+              Le taux d'occupation réel moyen des crèches
+            </p>
           </div>
-          <p style={texte}>Le taux d'occupation réel moyen des crèches</p>
         </div>
       </div>
     </div>
