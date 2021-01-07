f4ebe8fca74acf3904204bfc882ec2397433808d
SaKara69 <sabrina.karakog@gmail.com>
2021-01-07 17:45:23 +0100

    structure simulateur

--- components/Simulateur.js
+++ components/Simulateur.js
@@ -14,12 +14,33 @@ export default function Simulateur() {
           href="https://www.caf.fr/allocataires/droits-et-prestations/connaitre-vos-droits-selon-votre-situation/je-fais-garder-mon-enfant-dans-une-creche-ou-en-microcreche-la-caf-va-t-elle-m-aider#:~:text=Le%20montant%20de%20cette%20aide,pas%2085%20%25%20du%20montant%20total."
           target="_blank"
           rel="noreferrer"
-          className={styles.lien}
+          className={styles.lienPlusDinfos}
         >
           Plus d'informations
         </a>
       </div>
-      <div className={styles.simulateur} />
+      <div className={styles.simulateur}>
+        <div className={styles.critere}>
+          <h6 className={styles.critereTitre}>Nombres d'enfant(s) à charge</h6>
+          <div className={styles.nbEnfants}>
+            <p className={styles.chiffreEnfants}>1</p>
+            <p className={styles.chiffreEnfants}>2</p>
+            <p className={styles.chiffreEnfants}>3</p>
+            <p className={styles.chiffreEnfants}>4+</p>
+          </div>
+        </div>
+        <div className={styles.critere}>
+          <h6 className={styles.critereTitre}>Revenu net mensuel</h6>
+          <input />€
+        </div>
+        <div className={styles.critere}>
+          <h6 className={styles.critereTitre}>
+            Nombre d'heure(s) de garde par semaine
+          </h6>
+          <input /> heures
+        </div>
+        <button type="submit">Calculer</button>
+      </div>
     </div>
   );
 }
