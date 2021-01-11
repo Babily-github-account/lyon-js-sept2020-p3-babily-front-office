189dd6063bbc228adb470b01b9feba09f4be1f3a
christiantheobald <theobald.christian971@gmail.com>
2021-01-11 09:33:02 +0100

    espace pros

---
+++ pages/espacepro.js
@@ -0,0 +1,11 @@
+import CestSimpleAvecBabily from '../components/CestSimpleAvecBabily';
+import Layout from '../components/Layout';
+
+export default function EspacePro() {
+  return (
+    <Layout pageTitle="EspacePro">
+      <h1>Espace Professionnel</h1>
+      <CestSimpleAvecBabily />
+    </Layout>
+  );
+}
