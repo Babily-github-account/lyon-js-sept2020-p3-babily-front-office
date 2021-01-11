1f3b571321d6f646bc3535c4c8460f9e8fcb3ed1
Guillou33 <autiergilles@gmail.com>
2021-01-11 15:48:25 +0100

    add banieres

--- components/PresentationEspacePro.js
+++ components/PresentationEspacePro.js
@@ -9,7 +9,7 @@ export default function PresentationEspacePro() {
         <h1 className={styles.titlePres}>
           Vous êtes un professionnel de la petite enfance
         </h1>
-        <p>
+        <p className={styles.textPresPro}>
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
           commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
           et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
@@ -20,6 +20,7 @@ export default function PresentationEspacePro() {
           className={styles.avionPapier}
           src="/images/avionPapier.png"
           alt="Avion en papier"
+          layout="fixed"
           width={350}
           height={200}
         />
@@ -29,33 +30,37 @@ export default function PresentationEspacePro() {
           <div className={styles.titleBerceaux}>
             Les berceaux entreprises sont...
           </div>
-          <p>
-            Une <b>commercialisation complexe</b>
-          </p>
-          <p>
-            Des réservations à l’année, <b>sans garantie d’occupations</b>
-          </p>
-          <p>
-            Une gestion inévitable de <b>l’accueil occasionnel</b>
-          </p>
-          <p>
-            Une <b>charge administrative</b>, deportant les équipes de{' '}
-            <b>leur mission première</b>
-          </p>
+          <div className={styles.textBabily}>
+            <p>
+              Une <b>commercialisation complexe</b>
+            </p>
+            <p>
+              Des réservations à l’année, <b>sans garantie d’occupations</b>
+            </p>
+            <p>
+              Une gestion inévitable de <b>l’accueil occasionnel</b>
+            </p>
+            <p>
+              Une <b>charge administrative</b>, deportant les équipes de{' '}
+              <b>leur mission première</b>
+            </p>
+          </div>
         </div>
         <div className={styles.presBabily}>
           <div className={styles.titleBabily}>Aujourd'hui avec Babily :</div>
-          <p>
-            <b>Boostez vos clients entreprises</b> en répondant à leur besoin
-            réel
-          </p>
-          <p>
-            <b>Optimiser votre taux d’occupation</b> à l’aide de votre
-            calendrier partagé
-          </p>
-          <p>
-            <b>Recentrez votre équipe</b> sur le coeur de metier : bébé !
-          </p>
+          <div className={styles.textBabily}>
+            <p>
+              <b>Boostez vos clients entreprises</b> en répondant à leur besoin
+              réel
+            </p>
+            <p>
+              <b>Optimiser votre taux d’occupation</b> à l’aide de votre
+              calendrier partagé
+            </p>
+            <p>
+              <b>Recentrez votre équipe</b> sur le coeur de metier : bébé !
+            </p>
+          </div>
         </div>
       </section>
     </>
