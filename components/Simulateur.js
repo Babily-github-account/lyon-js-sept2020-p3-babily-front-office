21d9f010d813eee00fd6db61f89874815aa8651d
SaKara69 <sabrina.karakog@gmail.com>
2021-01-18 13:59:42 +0100

    suite merge dev

--- components/Simulateur.js
+++ components/Simulateur.js
@@ -2,7 +2,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
 
-import Link from 'next/link';
+// import Link from 'next/link';
 import {
   faInfoCircle,
   faClock,
