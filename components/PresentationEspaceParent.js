150af7085f7c0f572700c4bb2af5d04a19d8c21b
Guillou33 <autiergilles@gmail.com>
2021-01-18 14:21:32 +0100

    add espace parents and entreprises

---
+++ components/PresentationEspaceParent.js
@@ -0,0 +1,68 @@
+// import Link from 'next/link';
+// import Image from 'next/image';
+import styles from './PresentationEspacePro.module.css';
+
+export default function PresentationEspaceParent() {
+  return (
+    <>
+      <section className={styles.sectionPresEspacePro}>
+        <h1 className={styles.titlePres}>
+          Vous êtes un professionnel de la petite enfance
+        </h1>
+        <p className={styles.textPresPro}>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+          consequat massa quis enim.
+        </p>
+        <div className={styles.avionPapier} />
+      </section>
+      <section className={styles.sectionPresBabily}>
+        <div className={styles.presBerceaux}>
+          <div className={styles.titleBerceaux}>
+            Les berceaux entreprises sont...
+          </div>
+          <div className={styles.textBabily}>
+            <p>
+              Une <b>commercialisation complexe</b>
+            </p>
+            <hr className={styles.ligneRouge} />
+            <p>
+              Des réservations à l’année, <b>sans garantie d’occupations</b>
+            </p>
+            <hr className={styles.ligneRouge} />
+            <p>
+              Une gestion inévitable de <b>l’accueil occasionnel</b>
+            </p>
+            <hr className={styles.ligneRouge} />
+            <p>
+              Une <b>charge administrative</b>, deportant les équipes de{' '}
+              <b>leur mission première</b>
+            </p>
+          </div>
+        </div>
+        <div className={styles.presBabily}>
+          <div className={styles.titleBabily}>Aujourd'hui avec Babily :</div>
+          <div className={styles.textBabily}>
+            <p>
+              <b>Boostez vos clients entreprises</b> en répondant à leur besoin
+              réel
+            </p>
+            <hr className={styles.plusBleuHorizontal} />
+            <hr className={styles.plusBleuVertical} />
+            <p>
+              <b>Optimiser votre taux d’occupation</b> à l’aide de votre
+              calendrier partagé
+            </p>
+            <hr className={styles.plusBleuHorizontal} />
+            <hr className={styles.plusBleuVertical} />
+            <p>
+              <b>Recentrez votre équipe</b> sur le coeur de metier : bébé !
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
