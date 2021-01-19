0cf4da0263d930ed06d0c4bdce0b612ad1c259b6
gclement28 <clementgarcin6@gmail.com>
2021-01-19 11:08:11 +0100

    contact icons are done

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,11 @@
 import Layout from '../components/Layout';
+import Form from '../components/Form';
 
 export default function Contact() {
   return (
     <Layout pageTitle="Contact">
       <div>Contact</div>
+      <Form />
     </Layout>
   );
 }
--- pages/espaceparents.js
+++ pages/espaceparents.js
@@ -1,11 +1,13 @@
 import Layout from '../components/Layout';
 import Simulateur from '../components/Simulateur';
+import Form from '../components/Form';
 
 export default function EspaceParents() {
   return (
     <Layout pageTitle="EspaceParents">
       <h1>Espace Parents</h1>
       <Simulateur />
+      <Form />
     </Layout>
   );
 }
