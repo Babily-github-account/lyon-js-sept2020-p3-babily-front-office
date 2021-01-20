b24f251bcfadfe7121f390731d5a2c24e53b54ee
AmandineGervis <gervis.amandine@gmail.com>
2021-01-20 21:54:40 +0100

    responsiv decouvrir ok

--- pages/concept.js
+++ pages/concept.js
@@ -1,4 +1,5 @@
 import Layout from '../components/Layout';
+import HeaderConcept from '../components/HeaderConcept';
 import SuiteConcept from '../components/SuiteConcept';
 import Espaces from '../components/Espaces';
 import GrandTitreConcept from '../components/GrandTitreConcept';
@@ -7,52 +8,9 @@ import Information from '../components/Information';
 import LesChiffresBabilyConcept from '../components/LesChiffresBabilyConcept';
 
 export default function concept() {
-  const containerHead = {
-    backgroundImage: `url(/images/avion.png)`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    paddingTop: '12%',
-    paddingBottom: '12%',
-    filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
-  };
-  const conceptStart = {
-    width: '100%',
-  };
-
-  const titleHead = {
-    textAlign: 'center',
-    fontFamily: "'Montserrat Alternates', sans-serif",
-    color: 'white',
-    fontSize: '363%',
-  };
-
-  const containerSousTitle = {
-    margin: '5% auto',
-    backgroundImage: 'url(/images/converted/background4.webp)',
-    backgroundSize: 'cover',
-  };
-
-  const introduction = {
-    textAlign: 'center',
-    fontFamily: "'Montserrat Alternates', sans-serif",
-    fontSize: '1.5rem',
-    color: '#F5BD76',
-    paddingLeft: '20%',
-    paddingRight: '20%',
-  };
   return (
     <Layout pageTitle="Inscription">
-      <div style={conceptStart}>
-        <div style={containerHead}>
-          <h1 style={titleHead}>Découvrez Babily</h1>
-        </div>
-      </div>
-      <div style={containerSousTitle}>
-        <h5 style={introduction}>
-          “ Simplifions l’accueil des enfants par un véritable service aux
-          familles et une optimisation de la gestion des places ! ”
-        </h5>
-      </div>
+      <HeaderConcept />
       <SuiteConcept />
       <Espaces />
       <GrandTitreConcept />
