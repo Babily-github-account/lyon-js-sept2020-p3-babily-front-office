d7858f7c39960f9d63ab45735b52584771a17768
AmandineGervis <gervis.amandine@gmail.com>
2021-01-20 20:52:14 +0100

    responsiv middle page

--- components/GrandTitreConcept.js
+++ components/GrandTitreConcept.js
@@ -20,8 +20,6 @@ export default function GrandTitreConcept() {
             commodo ligula eget dolor. Aenean massa. Cum sociis natoque
             penatibus et magnis dis parturient montes, nascetur ridiculus mus.
             Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-            Nulla consequat massa quis enim. Lorem ipsum dolor sit amet,
-            consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
           </p>
         </div>
       </div>
@@ -35,9 +33,7 @@ export default function GrandTitreConcept() {
             Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
             Nulla consequat massa quis enim. Lorem ipsum dolor sit amet,
             consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-            Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
-            montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
-            pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+            Aenean massa.
           </p>
         </div>
         <div className={styles.containerImg}>
@@ -49,31 +45,28 @@ export default function GrandTitreConcept() {
             className={styles.photoBbPapa}
           />
         </div>
-        <div className={styles.containerParagraphe}>
-          <div className={styles.containerImg}>
-            <Image
-              src="/images/concept3.jpg"
-              alt="bebe qui dort"
-              width={700}
-              height={455}
-              className={styles.photoBbSecurite}
-            />
-          </div>
-          <div className={styles.containerSousTitre}>
-            <h3 className={styles.titleParagraphe}>En securité</h3>
-            <p className={styles.paragrapheConcept}>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-              Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
-              sem. Nulla consequat massa quis enim. Lorem ipsum dolor sit amet,
-              consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-              Aenean massa. Cum sociis natoque penatibus et magnis dis
-              parturient montes, nascetur ridiculus mus. Donec quam felis,
-              ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat
-              massa quis enim.
-            </p>
-          </div>
+      </div>
+      <div className={styles.containerParagraphe}>
+        <div className={styles.containerImg}>
+          <Image
+            src="/images/concept3.jpg"
+            alt="bebe qui dort"
+            width={700}
+            height={455}
+            className={styles.photoBbSecurite}
+          />
+        </div>
+        <div className={styles.containerSousTitre}>
+          <h3 className={styles.titleParagraphe}>En securité</h3>
+          <p className={styles.paragrapheConcept}>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim. Lorem ipsum dolor sit amet,
+            consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+            Aenean massa.
+          </p>
         </div>
       </div>
     </div>
