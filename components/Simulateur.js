2cc8dd967092a8f336bd794fd9013e3e87ce3f07
SaKara69 <sabrina.karakog@gmail.com>
2021-01-20 14:20:03 +0100

    intervention INDISPENSABLE DE GILLOU le gentil

--- components/Simulateur.js
+++ components/Simulateur.js
@@ -1,8 +1,9 @@
+/* eslint-disable radix */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-no-comment-textnodes */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import Link from 'next/link';
@@ -15,10 +16,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Simulateur.module.css';
 
 export default function Simulateur() {
+  const [resultatSimulateur, setResultatSimulateur] = useState(0);
   const { register, handleSubmit, errors } = useForm({
     mode: 'onTouched',
   });
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+    const nbrChildren = parseInt(data.children);
+    const revenuNetMensuel = parseInt(data.appointments);
+    const nbrHeures = parseInt(data.hours);
+    // Formule à demander a Nico le gentil
+    const resultat = (nbrChildren * revenuNetMensuel) / nbrHeures;
+    setResultatSimulateur(resultat);
+  };
+
+  const newSimulation = () => {
+    setResultatSimulateur(0);
+  };
 
   return (
     <div className={styles.containerSimulateur}>
@@ -54,160 +68,188 @@ export default function Simulateur() {
           Plus d'informations
         </a>
       </div>
-      <div className={styles.simulateur}>
-        <div className={styles.cercle1} />
-        <div className={styles.cercle2} />
-        <div className={styles.cercle3} />
-        <div className={styles.cercle4} />
-        <div className={styles.criteres}>
-          {/* ------------------------------------ CARTE RECHERCHE------------------------------------------- */}
-          <form className={styles.criteres} onSubmit={handleSubmit(onSubmit)}>
-            <div className={styles.critere}>
-              <label htmlFor="children" className={styles.critereTitre}>
-                Nombres d'enfant(s) à charge
-              </label>
-              <div className={styles.nbEnfants}>
-                <input
-                  type="radio"
-                  id="one"
-                  name="children"
-                  value="1"
-                  className={styles.demo2}
-                  ref={register({ required: true })}
-                />
-                <label htmlFor="one">1</label>
-                <input
-                  type="radio"
-                  id="two"
-                  name="children"
-                  value="2"
-                  className={styles.demo2}
-                  ref={register({ required: true })}
-                />
-                <label htmlFor="two">2</label>
-                <input
-                  type="radio"
-                  id="three"
-                  name="children"
-                  value="3"
-                  className={styles.demo2}
-                  ref={register({ required: true })}
-                />
-                <label htmlFor="three">3</label>
-                <input
-                  type="radio"
-                  id="four"
-                  name="children"
-                  value="4+"
-                  className={styles.demo2}
-                  ref={register({ required: true })}
-                />
-                <label htmlFor="four">4+</label>
-              </div>
-              {errors.children && (
-                <span className={styles.alertError}>
-                  Merci de sélectionner l'une des propositions.
-                </span>
-              )}
-            </div>
-            <div className={styles.critere}>
-              <label htmlFor="appointments" className={styles.critereTitre}>
-                Revenu net annuel{' '}
-                <FontAwesomeIcon
-                  icon={faInfoCircle}
-                  className={styles.iconeInfo}
-                />
-              </label>
-              <div className={styles.champ}>
-                <input
-                  type="number"
-                  id="appointments"
-                  name="appointments"
-                  ref={register({
-                    required: 'Merci de compléter ce champ',
-                    min: 0,
-                  })}
-                  className={styles.champSaisie}
-                />
-                <FontAwesomeIcon
-                  icon={faEuroSign}
-                  className={styles.iconeEuro}
-                />
-              </div>
-              {errors.appointments && (
-                <span className={styles.alertError}>
-                  Le revenu doit être renseigné et supérieur à 0.
-                </span>
-              )}
-            </div>
-            <div className={styles.critere}>
-              <label htmlFor="hours" className={styles.critereTitre}>
-                Nombre d'heure(s) de garde par semaine
-              </label>
-              <div className={styles.champ}>
-                <input
-                  type="number"
-                  id="hours"
-                  name="hours"
-                  ref={register({
-                    required: 'Merci de compléter ce champ',
-                    min: 1,
-                    max: 99,
-                  })}
-                  className={styles.champSaisie}
-                />{' '}
-                <FontAwesomeIcon icon={faClock} className={styles.iconeClock} />
-              </div>
-              {errors.hours && (
-                <span className={styles.alertError}>
-                  Le nombre d'heures doit être renseigné et compris entre 1 et
-                  99 heures.
-                </span>
-              )}
-            </div>
-            <button type="submit" name="calculer" className={styles.calculer}>
-              Calculer
-            </button>
-          </form>
-        </div>
-      </div>
-      <div>
-        <h2>Resultat taux d'effort:</h2>
-        {/* ------------------------------------ CARTE RESULTAT------------------------------------------- */}
-        <div className={styles.criteresResultat}>
-          <div className={styles.critere}>
-            <h5 className={styles.critereTitreResultat}>Cela vous coûterait</h5>
-            <p className={styles.critereTitre}>
-              <span className={styles.resultatEuroHeure}>0.90</span>
-              <FontAwesomeIcon
-                icon={faEuroSign}
-                className={styles.icones}
-              />{' '}
-              /heure
-            </p>
-            <div className={styles.resultatJour}>
-              <p className={styles.critereTitre}>
-                <span className={styles.critereTitreResultat}>soit </span>
-                <span className={styles.resultatEuroJour}> 8</span>
-                <FontAwesomeIcon
-                  icon={faEuroSign}
-                  className={styles.iconesEuro2}
-                />{' '}
-                /jour
-              </p>
+      {resultatSimulateur <= 0 ? (
+        <>
+          <div className={styles.simulateur}>
+            <div className={styles.cercle1} />
+            <div className={styles.cercle2} />
+            <div className={styles.cercle3} />
+            <div className={styles.cercle4} />
+            <div className={styles.criteres}>
+              {/* ------------------------------------ CARTE RECHERCHE------------------------------------------- */}
+
+              <form
+                className={styles.criteres}
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <div className={styles.critere}>
+                  <label htmlFor="children" className={styles.critereTitre}>
+                    Nombres d'enfant(s) à charge
+                  </label>
+                  <div className={styles.nbEnfants}>
+                    <input
+                      type="radio"
+                      id="one"
+                      name="children"
+                      value="1"
+                      className={styles.demo2}
+                      ref={register({ required: true })}
+                    />
+                    <label htmlFor="one">1</label>
+                    <input
+                      type="radio"
+                      id="two"
+                      name="children"
+                      value="2"
+                      className={styles.demo2}
+                      ref={register({ required: true })}
+                    />
+                    <label htmlFor="two">2</label>
+                    <input
+                      type="radio"
+                      id="three"
+                      name="children"
+                      value="3"
+                      className={styles.demo2}
+                      ref={register({ required: true })}
+                    />
+                    <label htmlFor="three">3</label>
+                    <input
+                      type="radio"
+                      id="four"
+                      name="children"
+                      value="4+"
+                      className={styles.demo2}
+                      ref={register({ required: true })}
+                    />
+                    <label htmlFor="four">4+</label>
+                  </div>
+                  {errors.children && (
+                    <span className={styles.alertError}>
+                      Merci de sélectionner l'une des propositions.
+                    </span>
+                  )}
+                </div>
+                <div className={styles.critere}>
+                  <label htmlFor="appointments" className={styles.critereTitre}>
+                    Revenu net annuel{' '}
+                    <FontAwesomeIcon
+                      icon={faInfoCircle}
+                      className={styles.iconeInfo}
+                    />
+                  </label>
+                  <div className={styles.champ}>
+                    <input
+                      type="number"
+                      id="appointments"
+                      name="appointments"
+                      ref={register({
+                        required: 'Merci de compléter ce champ',
+                        min: 0,
+                      })}
+                      className={styles.champSaisie}
+                    />
+                    <FontAwesomeIcon
+                      icon={faEuroSign}
+                      className={styles.iconeEuro}
+                    />
+                  </div>
+                  {errors.appointments && (
+                    <span className={styles.alertError}>
+                      Le revenu doit être renseigné et supérieur à 0.
+                    </span>
+                  )}
+                </div>
+                <div className={styles.critere}>
+                  <label htmlFor="hours" className={styles.critereTitre}>
+                    Nombre d'heure(s) de garde par semaine
+                  </label>
+                  <div className={styles.champ}>
+                    <input
+                      type="number"
+                      id="hours"
+                      name="hours"
+                      ref={register({
+                        required: 'Merci de compléter ce champ',
+                        min: 1,
+                        max: 60,
+                      })}
+                      className={styles.champSaisie}
+                    />{' '}
+                    <FontAwesomeIcon
+                      icon={faClock}
+                      className={styles.iconeClock}
+                    />
+                  </div>
+                  {errors.hours && (
+                    <span className={styles.alertError}>
+                      Le nombre d'heures doit être renseigné et compris entre 1
+                      et 60 heures.
+                    </span>
+                  )}
+                </div>
+                <button
+                  type="submit"
+                  name="calculer"
+                  className={styles.calculer}
+                >
+                  Calculer
+                </button>
+              </form>
             </div>
           </div>
-          <Link href="/">
-            <a href="" rel="noreferrer" className={styles.choisirCreche}>
-              Choisir ma crèche
-            </a>
-          </Link>
+        </>
+      ) : (
+        <>
+          <div>
+            <h2>Resultat taux d'effort:</h2>
+            {/* ------------------------------------ CARTE RESULTAT-------------------------------------------  */}
+            <div className={styles.criteresResultat}>
+              <div className={styles.critere}>
+                <h5 className={styles.critereTitreResultat}>
+                  Cela vous coûterait
+                </h5>
+                <p className={styles.critereTitre}>
+                  <span className={styles.resultatEuroHeure}>
+                    {resultatSimulateur.toFixed(2)}
+                  </span>
+                  <FontAwesomeIcon
+                    icon={faEuroSign}
+                    className={styles.icones}
+                  />{' '}
+                  /heure
+                </p>
+                <div className={styles.resultatJour}>
+                  <p className={styles.critereTitre}>
+                    <span className={styles.critereTitreResultat}>soit </span>
+                    <span className={styles.resultatEuroJour}>
+                      {(resultatSimulateur / 20).toFixed(2)}
+                    </span>
+                    <FontAwesomeIcon
+                      icon={faEuroSign}
+                      className={styles.iconesEuro2}
+                    />{' '}
+                    /jour
+                  </p>
+                </div>
+              </div>
+              <Link href="/">
+                <a href="" rel="noreferrer" className={styles.choisirCreche}>
+                  Choisir ma crèche
+                </a>
+              </Link>
 
-          <button type="button" className={styles.nouveauCalcul}>
-            Nouvelle simulation
-          </button>
-        </div>
-      </div>
+              <button
+                onClick={newSimulation}
+                type="button"
+                className={styles.nouveauCalcul}
+              >
+                Nouvelle simulation
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
