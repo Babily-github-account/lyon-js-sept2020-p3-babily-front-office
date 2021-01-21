ec6be3443f117a8f449903b75d1c557013aaebf6
AmandineGervis <gervis.amandine@gmail.com>
2021-01-21 09:27:41 +0100

    image radius

--- components/SuiteConcept.js
+++ components/SuiteConcept.js
@@ -22,15 +22,10 @@ export default function concept() {
         <svg viewBox="0 0 50 50" className={styles.palette}>
           <path d="M46.5,11.5c3.7,18.6-2.5,32-16.3,32S2.7,33,.4,21.2,5.3,4.9,18.9,2.2C29.8.1,43.5-4.2,46.5,11.5Z" />
         </svg>
-        <div className={styles.photoFilles}>
-          <Image
-            src="/images/jouet2.png"
-            alt="filles qui montrent le ciel"
-            width={600}
-            height={500}
-          />
-        </div>
       </div>
+      <svg viewBox="0 0 50 50" className={styles.palette1}>
+        <path d="M46.5,11.5c3.7,18.6-2.5,32-16.3,32S2.7,33,.4,21.2,5.3,4.9,18.9,2.2C29.8.1,43.5-4.2,46.5,11.5Z" />
+      </svg>
     </div>
   );
 }
