3e2b34f29186e6bb65d4ce7f3acc33ed0b7d4c30
Guillou33 <autiergilles@gmail.com>
2021-01-28 13:48:35 +0100

    code correction v1

--- components/PresentationEspacePro.js
+++ components/PresentationEspacePro.js
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-// import Image from 'next/image';
 import styles from './PresentationEspacePro.module.css';
 import CestSimpleAvecBabily from './CestSimpleAvecBabily';
 import Cards from './cards';
--- pages/vieprivee.js
+++
@@ -1,3 +0,0 @@
-export default function ViePrivee() {
-  return <>Vie privee</>;
-}
