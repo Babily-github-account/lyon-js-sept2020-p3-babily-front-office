03c41f0925be41881c07c9584b09ae45b06680f3
Guillou33 <autiergilles@gmail.com>
2021-02-01 14:32:39 +0100

    calendly in french and phone input for reminders

--- components/Form.js
+++ components/Form.js
@@ -2,6 +2,8 @@ import { useForm } from 'react-hook-form';
 import { useState } from 'react';
 import Recaptcha from 'react-google-recaptcha';
 import { InlineWidget } from 'react-calendly';
+import { Modal } from 'react-responsive-modal';
+import 'react-responsive-modal/styles.css';
 import styles from './Form.module.css';
 
 const Form = (props) => {
@@ -9,6 +11,7 @@ const Form = (props) => {
     mode: 'onChange',
   });
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const sujetForm = props;
 
   const sujetAPICalendly = (sujet) => {
@@ -32,76 +35,81 @@ const Form = (props) => {
     await setIsSubmitting(!isSubmitting);
   };
 
-  const onSubmit = (values) => {
+  const onSubmit = async (values) => {
     if (isSubmitting) {
       console.log(values);
+      await setIsOpen(true);
     }
-    setIsSubmitting(false);
   };
+
+  const onCloseModal = () => setIsOpen(false);
   return sujetAPICalendly(sujetForm.sujet) === 'email' ? (
-    <form className={styles.formContact} onSubmit={handleSubmit(onSubmit)}>
-      <h1 className={styles.formTitle}>Envoyez un mail à Babily</h1>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <div className={styles.formLeftColumn}>
-          <input
-            className={styles.leftInput}
-            placeholder="nom"
-            name="nom"
-            ref={register}
-            required
-          />
-          {errors.nom && (
-            <span className={styles.alertError}>Le nom est obligatoire</span>
-          )}
-          <input
-            className={styles.leftInput}
-            placeholder="email"
-            name="email"
-            ref={register({
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: 'Addresse mail invalide.',
-              },
-            })}
-            required
-          />
-          {errors.email && (
-            <span className={styles.alertError}>{errors.email.message}</span>
-          )}
-          <select className={styles.leftInput} name="gender" ref={register}>
-            <option value="creche">Crèche</option>
-            <option value="parent">Parent</option>
-            <option value="entreprise">Entreprise</option>
-          </select>
-          <Recaptcha
-            sitekey={siteKey}
-            ref={register}
-            size="normal"
-            onChange={executeCaptcha}
-          />
+    <>
+      <form className={styles.formContact} onSubmit={handleSubmit(onSubmit)}>
+        <h1 className={styles.formTitle}>Envoyez un mail à Babily</h1>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div className={styles.formLeftColumn}>
+            <input
+              className={styles.leftInput}
+              placeholder="nom"
+              name="nom"
+              ref={register}
+              required
+            />
+            {errors.nom && (
+              <span className={styles.alertError}>Le nom est obligatoire</span>
+            )}
+            <input
+              className={styles.leftInput}
+              placeholder="email"
+              name="email"
+              ref={register({
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Addresse mail invalide.',
+                },
+              })}
+              required
+            />
+            {errors.email && (
+              <span className={styles.alertError}>{errors.email.message}</span>
+            )}
+            <select className={styles.leftInput} name="gender" ref={register}>
+              <option value="creche">Crèche</option>
+              <option value="parent">Parent</option>
+              <option value="entreprise">Entreprise</option>
+            </select>
+            <Recaptcha
+              sitekey={siteKey}
+              ref={register}
+              size="normal"
+              onChange={executeCaptcha}
+            />
+          </div>
+          <div className={styles.formRightColumn}>
+            <textarea
+              className={styles.rightInput}
+              name="message"
+              placeholder="Ecrivez votre message ici"
+              ref={register}
+              required
+            />
+            {errors.message && (
+              <span className={styles.alertError}>Le nom est obligatoire</span>
+            )}
+          </div>
         </div>
-        <div className={styles.formRightColumn}>
-          <textarea
-            className={styles.rightInput}
-            name="message"
-            placeholder="Ecrivez votre message ici"
-            ref={register}
-            required
-          />
-          {errors.message && (
-            <span className={styles.alertError}>Le nom est obligatoire</span>
-          )}
-        </div>
-      </div>
-      <input type="submit" className={styles.inputSubmit} />
-    </form>
+        <input type="submit" className={styles.inputSubmit} />
+      </form>
+      <Modal open={isOpen} onClose={onCloseModal} center>
+        <h2>Message envoyé</h2>
+      </Modal>
+    </>
   ) : (
     <div className={styles.formContact}>
       <h1 className={styles.formTitle}>Planifiez votre rendez-vous :</h1>
       <InlineWidget
-        url={`https://calendly.com/autiergilles/${sujetAPICalendly(
-          sujetForm.sujet
-        )}`}
+        url={`https://calendly.com/testag/${sujetAPICalendly(sujetForm.sujet)}`}
         styles={{
           height: '1000px',
           width: '100%',
